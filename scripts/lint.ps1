db957441c49f72f484108f99d94fc92b108d9aaf
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-02-25 15:58:20 -0600

    Sync scripts

--- scripts/lint.ps1
+++ scripts/lint.ps1
@@ -1,5 +1,9 @@
 .venv\Scripts\activate
 
-python -m black app
+python -m black --line-length 120 --exclude migrations app
 
 python -m flake8 app
+
+python -m bandit --recursive --quiet --exclude test,migrations app
+
+python -m reorder_python_imports
